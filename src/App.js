d398860a172d65e7f02310bb1c428136199aba9c
Martin Zannato <real.jinx.studios@gmail.com>
2021-12-20 10:48:50 +0200

    all the shit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Fuck off there u shit.</p>
+        <h1 style={{ margin: 0 }}>GOD DAMN IT ALL TO HELL</h1>
+        <small>I think simply nobody really cares</small>
+        <br />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          nobody I want to I guess
+        </a>
+        <h3>how do i become important?</h3>
+        <h4>I guess I need to do something worthy.</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
